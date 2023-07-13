36de284cd0c7168675594cd6f959313eacd5d1b4
federico <feferreyra98@gmail.com>
2023-07-13 15:54:36 -0300

    Modifica referencia de Mongo database para utilizar Mongo Atlas (mas facil el deploy)

--- DatabaseContext/Context/MongoDataContext.cs
+++ DatabaseContext/Context/MongoDataContext.cs
@@ -1,5 +1,4 @@
 using ecommerce.DatabaseContext.Context.Interface;
-using Microsoft.Extensions.Configuration;
 using MongoDB.Driver;
 
 namespace ecommerce.DatabaseContext.Context;
@@ -9,13 +8,18 @@ public class MongoDataContext : IConnection<IMongoDatabase>
     private MongoClient _client;
 
     private string _dataBaseName;
+    private string _connectionString;
 
     public MongoDataContext()
     {
-        _client = new MongoClient("mongodb://127.0.0.1:45268");
-        _dataBaseName = "ecommerceDb";
+        _connectionString =
+            "mongodb+srv://grupo3:1234@clusterecommerce.bxf9npm.mongodb.net/?retryWrites=true&w=majority";
+        var settings = MongoClientSettings.FromConnectionString(_connectionString);
+        settings.ServerApi = new ServerApi(ServerApiVersion.V1);
+        _client = new MongoClient(settings);
+        _dataBaseName = "Ecommerce";
     }
-
+    
     public IMongoDatabase GetConnection()
     {
         return _client.GetDatabase(_dataBaseName);
